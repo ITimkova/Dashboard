cd7b25badcebdf5d9150ad71a90477406caed156
ITimkova <ivanka21timkova@gmail.com>
2023-04-18 20:45:35 +0200

    First Commit

---
+++ Infotech/DevexpressForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Infotech
+{
+    partial class DevexpressForm 
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SideBySideBarSeriesView sideBySideBarSeriesView1 = new DevExpress.XtraCharts.SideBySideBarSeriesView();
+            DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
+            DevExpress.XtraCharts.XYDiagram xyDiagram2 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SideBySideBarSeriesView sideBySideBarSeriesView2 = new DevExpress.XtraCharts.SideBySideBarSeriesView();
+            DevExpress.XtraCharts.Series series3 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series4 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.DoughnutSeriesView doughnutSeriesView1 = new DevExpress.XtraCharts.DoughnutSeriesView();
+            DevExpress.XtraCharts.ChartTitle chartTitle2 = new DevExpress.XtraCharts.ChartTitle();
+            this.countriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.infotechDatabaseDataSet = new Infotech.InfotechDatabaseDataSet();
+            this.adventureWorks2019DataSet = new Infotech.AdventureWorks2019DataSet();
+            this.salesTerritoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.salesTerritoryTableAdapter = new Infotech.AdventureWorks2019DataSetTableAdapters.SalesTerritoryTableAdapter();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.countriesTableAdapter = new Infotech.InfotechDatabaseDataSetTableAdapters.CountriesTableAdapter();
+            this.chartControl2 = new DevExpress.XtraCharts.ChartControl();
+            this.chartControl3 = new DevExpress.XtraCharts.ChartControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            ((System.ComponentModel.ISupportInitialize)(this.countriesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infotechDatabaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesTerritoryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(doughnutSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // countriesBindingSource
+            // 
+            this.countriesBindingSource.DataMember = "Countries";
+            this.countriesBindingSource.DataSource = this.infotechDatabaseDataSet;
+            // 
+            // infotechDatabaseDataSet
+            // 
+            this.infotechDatabaseDataSet.DataSetName = "InfotechDatabaseDataSet";
+            this.infotechDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // adventureWorks2019DataSet
+            // 
+            this.adventureWorks2019DataSet.DataSetName = "AdventureWorks2019DataSet";
+            this.adventureWorks2019DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // salesTerritoryBindingSource
+            // 
+            this.salesTerritoryBindingSource.DataMember = "SalesTerritory";
+            this.salesTerritoryBindingSource.DataSource = this.adventureWorks2019DataSet;
+            // 
+            // salesTerritoryTableAdapter
+            // 
+            this.salesTerritoryTableAdapter.ClearBeforeFill = true;
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.AccessibleName = "";
+            this.chartControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.chartControl1.DataSource = this.countriesBindingSource;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Center;
+            this.chartControl1.Legend.AlignmentVertical = DevExpress.XtraCharts.LegendAlignmentVertical.TopOutside;
+            this.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.Location = new System.Drawing.Point(22, 48);
+            this.chartControl1.Name = "chartControl1";
+            series1.ArgumentDataMember = "country";
+            series1.Name = "Series 1";
+            series1.ValueDataMembersSerializable = "revenue";
+            sideBySideBarSeriesView1.Border.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            series1.View = sideBySideBarSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.SeriesTemplate.ArgumentDataMember = "country";
+            this.chartControl1.SeriesTemplate.ColorDataMember = "country";
+            this.chartControl1.SeriesTemplate.ValueDataMembersSerializable = "revenue";
+            this.chartControl1.Size = new System.Drawing.Size(608, 263);
+            this.chartControl1.TabIndex = 0;
+            chartTitle1.DXFont = new DevExpress.Drawing.DXFont("Tahoma", 25F);
+            chartTitle1.Indent = 13;
+            chartTitle1.MaxLineCount = 1;
+            chartTitle1.Text = "Revenue by country";
+            chartTitle1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.chartControl1.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
+            chartTitle1});
+            // 
+            // countriesTableAdapter
+            // 
+            this.countriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // chartControl2
+            // 
+            xyDiagram2.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram2.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl2.Diagram = xyDiagram2;
+            this.chartControl2.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right;
+            this.chartControl2.Location = new System.Drawing.Point(636, 48);
+            this.chartControl2.Name = "chartControl2";
+            this.chartControl2.PaletteName = "Concourse";
+            series2.ArgumentDataMember = "country";
+            series2.DataSource = this.countriesBindingSource;
+            series2.Name = "Projects Planned";
+            series2.ValueDataMembersSerializable = "projectsPlanned";
+            sideBySideBarSeriesView2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(204)))), ((int)(((byte)(228)))));
+            series2.View = sideBySideBarSeriesView2;
+            series3.ArgumentDataMember = "country";
+            series3.DataSource = this.countriesBindingSource;
+            series3.Name = "Projects Completed";
+            series3.ValueDataMembersSerializable = "projectsCompleted";
+            this.chartControl2.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series2,
+        series3};
+            this.chartControl2.Size = new System.Drawing.Size(696, 337);
+            this.chartControl2.TabIndex = 1;
+            this.chartControl2.SizeChanged += new System.EventHandler(this.DevexpressForm_Load);
+            this.chartControl2.DoubleClick += new System.EventHandler(this.chartControl2_DoubleClick);
+            this.chartControl2.Resize += new System.EventHandler(this.DevexpressForm_Load);
+            // 
+            // chartControl3
+            // 
+            this.chartControl3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.chartControl3.DataSource = this.infotechDatabaseDataSet;
+            this.chartControl3.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right;
+            this.chartControl3.Legend.Border.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            this.chartControl3.Legend.MarkerSize = new System.Drawing.Size(20, 10);
+            this.chartControl3.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl3.Location = new System.Drawing.Point(23, 316);
+            this.chartControl3.Name = "chartControl3";
+            series4.ArgumentDataMember = "country";
+            series4.DataSource = this.countriesBindingSource;
+            series4.LabelsVisibility = DevExpress.Utils.DefaultBoolean.True;
+            series4.Name = "Series 1";
+            series4.ValueDataMembersSerializable = "revenue";
+            series4.View = doughnutSeriesView1;
+            this.chartControl3.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series4};
+            this.chartControl3.SeriesTemplate.ArgumentDataMember = "Countries.country";
+            this.chartControl3.SeriesTemplate.ValueDataMembersSerializable = "Countries.revenue";
+            this.chartControl3.Size = new System.Drawing.Size(300, 200);
+            this.chartControl3.TabIndex = 2;
+            chartTitle2.Text = "Revenue by country";
+            chartTitle2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            chartTitle2.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            this.chartControl3.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
+            chartTitle2});
+            this.chartControl3.Resize += new System.EventHandler(this.DevexpressForm_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.textBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox1.Location = new System.Drawing.Point(557, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 25);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "DASHBOARD";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // barManager1
+            // 
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.MaxItemId = 26;
+            this.barManager1.ShowScreenTipsInMenus = true;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(1374, 0);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 545);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(1374, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 545);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(1374, 0);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 545);
+            // 
+            // DevexpressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1374, 545);
+            this.Controls.Add(this.chartControl2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chartControl3);
+            this.Controls.Add(this.chartControl1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.MinimumSize = new System.Drawing.Size(1376, 576);
+            this.Name = "DevexpressForm";
+            this.Text = "DevexpressForm";
+            this.Load += new System.EventHandler(this.DevexpressForm_Load);
+            this.Resize += new System.EventHandler(this.DevexpressForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.countriesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infotechDatabaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salesTerritoryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(doughnutSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private AdventureWorks2019DataSet adventureWorks2019DataSet;
+        private System.Windows.Forms.BindingSource salesTerritoryBindingSource;
+        private AdventureWorks2019DataSetTableAdapters.SalesTerritoryTableAdapter salesTerritoryTableAdapter;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private InfotechDatabaseDataSet infotechDatabaseDataSet;
+        private System.Windows.Forms.BindingSource countriesBindingSource;
+        private InfotechDatabaseDataSetTableAdapters.CountriesTableAdapter countriesTableAdapter;
+        private DevExpress.XtraCharts.ChartControl chartControl2;
+        private DevExpress.XtraCharts.ChartControl chartControl3;
+        private System.Windows.Forms.TextBox textBox1;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+    }
+}
---
+++ Infotech/DevexpressForm.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.DashboardWin;
+using DevExpress.XtraCharts;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Infotech
+{
+    public partial class DevexpressForm : DevExpress.XtraEditors.XtraForm
+    {
+        private Size formOriginalSize;
+        private Rectangle recChartControl1;
+        private Rectangle recChartControl2;
+        private Rectangle recChartControl3;
+        private Rectangle recTextBox1;
+
+        bool chartControl2IsZoomed = false;
+
+        public DevexpressForm()
+        {
+            InitializeComponent();
+            this.Resize += DevexpressForm_Resize;
+            formOriginalSize = this.Size;
+            recChartControl1 = new Rectangle(chartControl1.Location, chartControl1.Size);
+            recChartControl2 = new Rectangle(chartControl2.Location, chartControl2.Size);
+            recChartControl3 = new Rectangle(chartControl3.Location, chartControl3.Size);
+            recTextBox1 = new Rectangle(textBox1.Location, textBox1.Size);
+
+        }
+
+
+        private void resize_Control(Control c, Rectangle r)
+        {
+            float xRatio = (float)(this.Width) / (float)(formOriginalSize.Width);
+            float yRatio = (float)(this.Height) / (float)(formOriginalSize.Height);
+            int newX = (int)(r.X * xRatio);
+            int newY = (int)(r.Y * yRatio);
+
+            int newWidth = (int)(r.Width * xRatio);
+            int newHeight = (int)(r.Height * yRatio);
+
+            c.Location = new Point(newX, newY);
+            c.Size = new Size(newWidth, newHeight);
+        }
+
+        private void DevexpressForm_Resize(object sender, EventArgs e)
+        {
+            resize_Control(chartControl1, recChartControl1);
+            resize_Control(chartControl2, recChartControl2);
+            resize_Control(chartControl3, recChartControl3);
+            resize_Control(textBox1, recTextBox1);
+
+        }
+        private void DevexpressForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'infotechDatabaseDataSet.Countries' table. You can move, or remove it, as needed.
+            this.countriesTableAdapter.Fill(this.infotechDatabaseDataSet.Countries);
+
+
+        }
+
+        private void chartControl2_DoubleClick(object sender, EventArgs e)
+        {
+            if (chartControl2IsZoomed == false)
+            {
+                chartControl2.Dock = DockStyle.Fill;
+                chartControl2IsZoomed = true;
+            }
+            else
+            {
+                chartControl2.Dock = DockStyle.None;
+                chartControl2IsZoomed = false;
+            }
+
+           
+        }
+
+        
+    }
+}
